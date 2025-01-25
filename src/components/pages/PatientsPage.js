459d2d77281f1d8def144f740c271862b6e459e6
Tomás <iglesiastomas99@gmail.com>
2025-01-24 21:16:48 -0300

    fetch patients

--- src/components/pages/PatientsPage.js
+++ src/components/pages/PatientsPage.js
@@ -1,11 +1,32 @@
+import { useState, useEffect } from 'react';
+import api from '../../api/axiosConfig';
+
 const PatientsPage = () => {
-    return (
-      <div>
-        <h1>Patients</h1>
-        <p>List of patients will go here.</p>
-      </div>
-    );
+  const [patients, setPatients] = useState([]);
+
+  const fetchPatients = async () => {
+    try {
+      const response = await api.get("/patients");
+      setPatients(response.data);
+    } catch (error) {
+      console.error("Error fetching patients:", error);
+    }
   };
-  
-  export default PatientsPage;
-  
+
+  useEffect(() => {
+    fetchPatients();
+  }, []);
+
+  return (
+    <div>
+      <h1>Pacientes</h1>
+      <ul>
+        {patients.map((patient) => (
+          <li key={patient.id}>{patient.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default PatientsPage;
