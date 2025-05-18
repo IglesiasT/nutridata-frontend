e059de5482c05c6045ddd54bf75c421940ca215d
Tomás <iglesiastomas99@gmail.com>
2025-05-18 20:59:21 -0300

    mock auth service

---
+++ src/services/authServiceFactory.js
@@ -0,0 +1,8 @@
+import realAuthService from './authService';
+import mockAuthService from './MockAuthService';
+
+const USE_MOCK = process.env.REACT_APP_USE_MOCK === 'true' || false;
+
+// Export the appropriate authentication service based on the environment variable
+const authServiceFactory = USE_MOCK ? mockAuthService : realAuthService;
+export default authServiceFactory;
