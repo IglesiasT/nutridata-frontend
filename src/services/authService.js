75d5fef97786d50e54b0141b6d3f5c1a66b50606
Tomás <iglesiastomas99@gmail.com>
2025-05-28 22:53:12 -0300

    fix keycloak port

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,7 +1,7 @@
 import Keycloak from 'keycloak-js';
 
 const keycloakConfig = {
-  url: 'http://localhost:8080',
+  url: 'http://localhost:8181',
   realm: 'nutridata-realm',
   clientId: 'nutridata_client'
 };
